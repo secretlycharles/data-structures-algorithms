6e01d81171e8b228a06283baef3ff2bf17764202
secretlycharles <imcharlesmendez@gmail.com>
2025-05-04 20:50:01 -0700

    2 dim arrays recommented

--- learning-c/array-multidimensional/main.c
+++ learning-c/array-multidimensional/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    char arr [2][3] = {
-        { 's', 'k', 'i' },
-        { 'b', 'i', 'd' },
+    char arr [3][4] = {
+        { 's', 'k', 'i', },
+        { 'b', 'i', 'd', },
+        { 'i', ' ', ' '  }
     };
 
-    // Loop over current arrays
+    // fetch array sizes
     int rows = sizeof(arr)/sizeof(arr[0]);
     int columns = sizeof(arr[0])/sizeof(arr[0][0]);
     
+    // Loop over current arrays
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < columns; j++) {
             printf("%c", arr[i][j]);
